bc2cbb6ebdb73ea19525b74fba39538ce22a8264
Eugene Heckert <eugene.heckert@bottlerocketstudios.com>
2017-10-26 15:40:51 -0500

    switching over to my custom shellout for testing

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ import PackageDescription
 let package = Package(
     name: "Releases",
     dependencies: [
-        .Package(url: "https://github.com/JohnSundell/ShellOut.git", majorVersion: 1),
+        .Package(url: "https://github.com/eugeneheckert/ShellOut.git", majorVersion: 1),
         .Package(url: "https://github.com/JohnSundell/Require.git", majorVersion: 1)
     ]
 )
